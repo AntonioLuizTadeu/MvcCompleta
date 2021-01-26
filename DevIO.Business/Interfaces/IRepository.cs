27f5a1004b44d067168f9a59adb9e416cc414150
AntonioLuizTadeu <30487304+AntonioLuizTadeu@users.noreply.github.com>
2021-01-25 22:11:52 -0300

    Add files via upload

---
+++ DevIO.Business/Interfaces/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IRepository<TEntyti> : IDisposable where TEntyti : Entity
+    {
+
+        Task Adicionar(TEntyti entyti);
+
+        Task<TEntyti> ObterPorId(Guid id);
+
+        Task<List<TEntyti>> ObterTodos();
+
+        Task Atualizar(TEntyti entyti);
+
+        Task Remover(Guid id);
+
+        Task<IEnumerable<TEntyti>> Buscar(Expression<Func<TEntyti, bool>> predicate);
+
+        Task<int> SaveChanges();
+
+    }
+}
---
+++ DevIO.data/Migrations/20210125002451_MeuDbContxt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevIO.data.Migrations
+{
+    public partial class MeuDbContxt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Enderecos",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varcgar(50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Enderecos",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+        }
+    }
+}
